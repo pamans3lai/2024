a4cffdb29360fdf191bd425f7a4ad40b91732d24
pamans3lai <pamans3lai@gmail.com>
2024-02-01 10:53:51 +0700

    1/366

---
+++ 1-reducebutgrow.js
@@ -0,0 +1,13 @@
+// // WPU Codewar
+// 1/366
+// // https://www.codewars.com/kata/57f780909f7e8e3183000078/train/javascript
+
+
+function grow(x) {
+  let result = x[0];
+  for (let i = 1; i < x.length; i++) {
+    result = result * x[i];
+  }
+  return result;
+}
+console.log(grow([1,2,3]));
