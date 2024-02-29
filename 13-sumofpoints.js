0e4c8eb8b1206cad84a7643483d6e6b1d83935bb
pamans3lai <pamans3lai@gmail.com>
2024-02-29 18:00:16 +0700

    15

--- 13-sumofpoints.js
+++ 13-sumofpoints.js
@@ -1,2 +1,14 @@
 # 13/366
 # https://www.codewars.com/kata/5bb904724c47249b10000131
+
+function points(games) {
+    let result = 0;
+    for (let i = 0; i < games.length; i++) {
+      if (games[i][0] > games[i][2]) {
+        result += 3;
+      } else if (games[i][0] === games[i][2]) {
+        result += 1;
+      }
+    }
+    return result;
+  }
